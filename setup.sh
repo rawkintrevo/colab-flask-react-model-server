24e5f30915a59bc77c65d96067047ef9feacf68e
rawkintrevo <trevor.d.grant@gmail.com>
2023-11-10 16:39:28 -0600

    bug bashed

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt update -q
 sudo apt install nodejs npm -q
 npm install -g n
 n stable
+sudo apt remove python3-blinker
 pip install -q -U -r requirements.txt
 (cd react-app; npm install; npm run build)
 
