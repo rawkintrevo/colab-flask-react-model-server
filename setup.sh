1ca5bddab023ba7bc4140e1a71d8f5baa1b111e2
rawkintrevo <trevor.d.grant@gmail.com>
2023-11-10 16:12:56 -0600

    typo

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt update -q
 sudo apt install nodejs npm -q
 npm install -g n
 n stable
-pip install -q -U -r requirement.txt
+pip install -q -U -r requirements.txt
 (cd react-app; npm install; npm run build)
 
 
